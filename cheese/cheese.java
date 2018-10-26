b2a2d6df2a89680ffdb9d60b532c230f77ee7c4f
Daniel Koren <danielkoren@Daniels-MacBook-Pro.local>
2018-10-26 18:05:47 +0300

    Removed unneeded

--- cheese/cheese.java
+++
@@ -1,6 +0,0 @@
-int roomInBelly = 5;
-public void eatCheese(int bitesOfCheese) {
-while (bitesOfCheese > 0 && roomInBelly > 0) {
-bitesOfCheese--;
-roomInBelly--; }
-System.out.println(bitesOfCheese+" pieces of cheese left"); }
--- variableTypeTask/Price.java
+++
@@ -1,7 +0,0 @@
-public class Price {
-	public void admission() {
-		double amount = 0xE;
-
-	System.out.println(amount);
-
-} }
