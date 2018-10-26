8724be0531009ede4e37e7e847f34979c81bf47d
danielkrn <daniels.korens@gmail.com>
2018-10-26 22:06:14 +0300

    Update helloWorld.java

--- helloWorld/helloWorld.java
+++ helloWorld/helloWorld.java
@@ -2,7 +2,7 @@ public class helloWorld {
 
 public static void main(String[] args) {
 
-	System.out.print("Hello World!");
+	System.out.print("Hello World!");	//Probably the simplest Java Hello World! app.
 	}
 
 }
